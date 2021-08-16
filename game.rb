6aa6997f7d543507fb8961fb69e3d4f5751278a1
mohammedpythons <mohammedshokai99@gmail.com>
2021-08-16 17:27:25 -0400

    fix a bug

--- game.rb
+++ game.rb
@@ -11,12 +11,11 @@ class Game
         # @winner = nil
     end
     def start_game
-        puts @counter
         until @game_status == 0
-            # puts "hey i'm here while loop"
+
             @player = @players[@counter % 2]
             @other_player = @players[(@counter + 1) % 2]
-            # ask_question(player.name)
+
             question = Question.new
             print " #{@player.name}: what does #{question.num1} + #{question.num2} equal? \n"
             input = gets.chomp.to_i
@@ -24,7 +23,16 @@ class Game
             if input != question.answer
                 puts "#{@player.name} Seriously? No!"
                 @player.reduce_life
-                check_life
+                if check_life
+                    @game_status = 0
+
+                    puts "#{@other_player.name} is the winner with a score of #{@other_player.lives}/3 "
+                    puts "---GAME OVER---"
+                    puts "Good bye!"
+                else
+                puts "p1 #{@players[0].lives}/3 VS P2 #{@players[1].lives}/3"
+                puts "----NEW TURN----"
+                end
             else
                 puts "#{@player.name} YES! You are correct!"
                 puts "p1 #{@players[0].lives}/3 VS P2 #{@players[1].lives}/3"
@@ -37,12 +45,9 @@ class Game
 
 
     def check_life
-        if @player.lives == 0
-            @game_status = 0
 
-            puts "#{@player.name} is the winner"
-            puts "---GAME OVER---"
-            puts "Good bye!"
+        if @player.lives == 0
+            return true
         end
 
     end
@@ -53,12 +58,6 @@ class Game
 
 
 
-    # def get_current_player
-    #     if counter.remainder(2) == 0
-    #        return @players[0]
-    #     else
-    #        return @players[1]
-    #     end
-    # end
+
 
 end
